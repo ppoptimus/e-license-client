ec148a33b5e0b356627488c118dfa128f8c6625d
rpumpo <r.pumpo@gmail.com>
2022-12-14 18:35:03 +0700

    first

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import "./assets/scss/themes.scss";
+import { Header } from "./Menu/Header";
+import SideBar from "./Menu/Sidebar";
 
 function App() {
+  const [headerClass, setHeaderClass] = useState("");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <div id="layout-wrapper">
+        <Header />
+        <SideBar />
+      </div>
+    </React.Fragment>
   );
 }
 
